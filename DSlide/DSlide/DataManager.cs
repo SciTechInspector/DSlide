0c90cfebbdcbcfcec7d777baa175e918d2772e5f
SciTechInvestigator <SciTechInvestigator@hotmail.com>
2021-09-14 04:37:01 +0200

    Initial implementation of DSlide. Get and Set of simple properties are now working.

---
+++ DSlide/DSlide/DataManager.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DSlide
+{
+    public class DataManager
+    {
+        DataVersion currentVersion;
+
+        Dictionary<DataNodeKey, DataNode> allDataNodes = new Dictionary<DataNodeKey, DataNode>();
+        List<DataNode> sourceDataNodes = new List<DataNode>();
+
+        public void SetSourceValue<T>(T newValue, object container, string propertyName, Action notifier)
+        {
+            DataNode dataNode;
+            var dataNodeKey = new DataNodeKey(container, propertyName);
+            if (!this.allDataNodes.TryGetValue(dataNodeKey, out dataNode))
+            {
+                dataNode = new DataNode(dataNodeKey);
+                this.allDataNodes[dataNodeKey] = dataNode;
+                this.sourceDataNodes.Add(dataNode);
+            }
+
+            dataNode.SetValue(newValue, currentVersion);
+        }
+
+        public T GetSourceValue<T>(object container, string propertyName, Action notifier) 
+        {
+            DataNode dataNode;
+            var dataNodeKey = new DataNodeKey(container, propertyName);
+            if (!this.allDataNodes.TryGetValue(dataNodeKey, out dataNode))
+            {
+                return default(T);
+            }
+
+            var retrievedValue = dataNode.GetValue(currentVersion);
+            if (retrievedValue == null)
+                return default(T);
+
+            return (T)retrievedValue;
+        }
+
+        public T GetComputedValue<T>(Func<T> computer, object container, string propertyName, Action notifier) 
+        {
+            return default(T);
+        }
+    }
+}
---
+++ DSlide/DSlide/DataVersion.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace DSlide
+{
+    public struct DataVersion : IEquatable<DataVersion>, IComparable, IComparable<DataVersion>
+    {
+        public long VersionNumber { get; set; }
+        public DataManager DataManager { get; set; }
+
+        public int CompareTo(object obj)
+        {
+            if (obj == null)
+            {
+                throw new NullReferenceException("Cannot compare Version to a null reference.");
+            }
+
+            if (!(obj is DataVersion))
+            {
+                throw new InvalidOperationException("Cannot compare Version to " + obj.GetType().Name);
+            }
+
+            return this.CompareTo((DataVersion)obj);
+        }
+
+        public int CompareTo(DataVersion other)
+        {
+            if (other.DataManager != this.DataManager)
+            {
+                throw new InvalidOperationException("Cannot compare two DataVersions from two different data domains.");
+            }
+
+            if (other.VersionNumber == this.VersionNumber)
+                return 0;
+
+            if (other.VersionNumber > this.VersionNumber)
+                return 1;
+
+            Debug.Assert(other.VersionNumber < this.VersionNumber);
+            return 0;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is DataVersion version && Equals(version);
+        }
+
+        public bool Equals(DataVersion other)
+        {
+            return VersionNumber == other.VersionNumber &&
+                   EqualityComparer<DataManager>.Default.Equals(DataManager, other.DataManager);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(VersionNumber, DataManager);
+        }
+    }
+}
